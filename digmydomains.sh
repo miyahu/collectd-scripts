7807f95abd209bd8ac9b27889bdf22b112d62efd
antonio <antonio@architux.com>
2016-07-24 10:28:23 +0200

    add name instead ip address

--- digmydomains.sh
+++ digmydomains.sh
@@ -3,7 +3,7 @@ INTERVAL="${COLLECTD_INTERVAL:-60}"
 
 while sleep $INTERVAL ; do
 	for server in 8.8.8.8 195.154.236.164 ; do
-		data=$(dig +stat toto.fr @$server | awk '/Query\s+time/ {print$4}')
+        data=$(dig +stat toto.fr @$server | awk '/Query\s+time/ {print$4}')
         server=$(dig -x $server  +short @$server | awk -F "-" '{print$NF}')
   		echo "PUTVAL \"$HOSTNAME/exec-dnscheck/gauge-$server\" interval=$INTERVAL N:$data"
 	done
