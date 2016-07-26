73648e0228acf0724fac7dfb1474e53f3cff3fc2
Antonio Moscato <antonio.moscato@alterway.fr>
2016-07-26 11:29:40 +0200

    modify awk regex

--- digmydomains.sh
+++ digmydomains.sh
@@ -5,7 +5,7 @@ servers="8.8.8.8 195.154.236.164"
 
 while sleep $INTERVAL ; do
 	for server in $servers ; do
-        response_time=$(dig +stat toto.fr @$server | awk '/Query\s+time/ {print$4}')
+        response_time=$(dig +stat linuxfr.org @$server | awk '/Query\ time/ {print$4}')
         server_name=$(dig -x $server  +short @$server | awk -F "-" '{print$NF}')
         echo "PUTVAL \"$HOSTNAME/exec-dnscheck/response_time-$server_name\" interval=$INTERVAL N:$response_time"
 	done
